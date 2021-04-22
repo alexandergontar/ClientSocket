1bc7be8363d7d37395ab1c9b2590aed525136b73
alexandergontar <57796053+alexandergontar@users.noreply.github.com>
2021-04-22 09:32:40 +0300

    Add files via upload

---
+++ ClientSocket/ClientSocket/Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace ClientSocket
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioBtnOn = new System.Windows.Forms.RadioButton();
+            this.radioBtnOff = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioBtnAuto = new System.Windows.Forms.RadioButton();
+            this.textHostName = new System.Windows.Forms.TextBox();
+            this.portNum = new System.Windows.Forms.NumericUpDown();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.setTemp = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnSerial = new System.Windows.Forms.Button();
+            this.btnMonitor = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.portNum)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.setTemp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(36, 83);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(454, 28);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(423, 26);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioBtnOn
+            // 
+            this.radioBtnOn.AutoSize = true;
+            this.radioBtnOn.Location = new System.Drawing.Point(28, 27);
+            this.radioBtnOn.Name = "radioBtnOn";
+            this.radioBtnOn.Size = new System.Drawing.Size(121, 28);
+            this.radioBtnOn.TabIndex = 0;
+            this.radioBtnOn.Text = "Heater ON";
+            this.radioBtnOn.UseVisualStyleBackColor = true;
+            // 
+            // radioBtnOff
+            // 
+            this.radioBtnOff.AutoSize = true;
+            this.radioBtnOff.Checked = true;
+            this.radioBtnOff.Location = new System.Drawing.Point(28, 71);
+            this.radioBtnOff.Name = "radioBtnOff";
+            this.radioBtnOff.Size = new System.Drawing.Size(131, 28);
+            this.radioBtnOff.TabIndex = 0;
+            this.radioBtnOff.TabStop = true;
+            this.radioBtnOff.Text = "Heater OFF";
+            this.radioBtnOff.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioBtnAuto);
+            this.groupBox1.Controls.Add(this.radioBtnOff);
+            this.groupBox1.Controls.Add(this.radioBtnOn);
+            this.groupBox1.Location = new System.Drawing.Point(516, 136);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(185, 188);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Controls";
+            // 
+            // radioBtnAuto
+            // 
+            this.radioBtnAuto.AutoSize = true;
+            this.radioBtnAuto.Location = new System.Drawing.Point(28, 120);
+            this.radioBtnAuto.Name = "radioBtnAuto";
+            this.radioBtnAuto.Size = new System.Drawing.Size(70, 28);
+            this.radioBtnAuto.TabIndex = 1;
+            this.radioBtnAuto.TabStop = true;
+            this.radioBtnAuto.Text = "Auto";
+            this.radioBtnAuto.UseVisualStyleBackColor = true;
+            // 
+            // textHostName
+            // 
+            this.textHostName.Location = new System.Drawing.Point(87, 26);
+            this.textHostName.Name = "textHostName";
+            this.textHostName.Size = new System.Drawing.Size(129, 28);
+            this.textHostName.TabIndex = 7;
+            this.textHostName.Text = "192.168.0.41";
+            // 
+            // portNum
+            // 
+            this.portNum.Location = new System.Drawing.Point(287, 26);
+            this.portNum.Name = "portNum";
+            this.portNum.Size = new System.Drawing.Size(120, 28);
+            this.portNum.TabIndex = 8;
+            this.portNum.Value = new decimal(new int[] {
+            80,
+            0,
+            0,
+            0});
+            this.portNum.ValueChanged += new System.EventHandler(this.portNum_ValueChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 354);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(726, 28);
+            this.statusStrip1.TabIndex = 12;
+            this.statusStrip1.Text = "Status";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.toolStripStatusLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(60, 23);
+            this.toolStripStatusLabel1.Text = "Status";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(360, 136);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Padding = new System.Windows.Forms.Padding(5);
+            this.pictureBox1.Size = new System.Drawing.Size(130, 188);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(381, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 24);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "ON";
+            // 
+            // chart1
+            // 
+            this.chart1.BorderlineColor = System.Drawing.Color.RoyalBlue;
+            this.chart1.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
+            this.chart1.BorderlineWidth = 4;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(36, 136);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Color = System.Drawing.Color.Red;
+            series1.Legend = "Legend1";
+            series1.Name = "Temperature";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Humidity";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(300, 188);
+            this.chart1.TabIndex = 15;
+            this.chart1.Text = "Load Chart";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(381, 256);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 24);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "OFF";
+            // 
+            // setTemp
+            // 
+            this.setTemp.Location = new System.Drawing.Point(621, 83);
+            this.setTemp.Name = "setTemp";
+            this.setTemp.Size = new System.Drawing.Size(80, 28);
+            this.setTemp.TabIndex = 17;
+            this.setTemp.Value = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(512, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 24);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Set Temp.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 24);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Host";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(238, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 24);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Port";
+            // 
+            // btnSerial
+            // 
+            this.btnSerial.Location = new System.Drawing.Point(621, 13);
+            this.btnSerial.Name = "btnSerial";
+            this.btnSerial.Size = new System.Drawing.Size(93, 37);
+            this.btnSerial.TabIndex = 21;
+            this.btnSerial.Text = "WiFi";
+            this.btnSerial.UseVisualStyleBackColor = true;
+            this.btnSerial.Click += new System.EventHandler(this.btnSerial_Click);
+            // 
+            // btnMonitor
+            // 
+            this.btnMonitor.Location = new System.Drawing.Point(526, 26);
+            this.btnMonitor.Name = "btnMonitor";
+            this.btnMonitor.Size = new System.Drawing.Size(75, 39);
+            this.btnMonitor.TabIndex = 22;
+            this.btnMonitor.Text = "Mon";
+            this.btnMonitor.UseVisualStyleBackColor = true;
+            this.btnMonitor.Click += new System.EventHandler(this.btnMonitor_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 382);
+            this.Controls.Add(this.btnMonitor);
+            this.Controls.Add(this.btnSerial);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.setTemp);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.portNum);
+            this.Controls.Add(this.textHostName);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "ESP8266";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.portNum)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.setTemp)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioBtnOff;
+        private System.Windows.Forms.RadioButton radioBtnOn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textHostName;
+        private System.Windows.Forms.NumericUpDown portNum;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioBtnAuto;
+        private System.Windows.Forms.NumericUpDown setTemp;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnSerial;
+        private System.Windows.Forms.Button btnMonitor;
+    }
+}
+
---
+++ ClientSocket/ClientSocket/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+
+namespace ClientSocket
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient clientSocket;
+        private NetworkStream serverStream;
+        private byte[] outStream;
+        private string str = "LED=DISC";
+        private string IPadd;
+        private int tcp_port;
+        private int t;
+        private int h;
+        private bool on_off=false, mon=false;
+        private Bitmap bitmap;
+        private delegate void requestUnit(object sender, EventArgs e);
+        private event requestUnit req;
+
+        public Serial s_conn;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormClosing += ProgramClose;
+            req += new requestUnit(button1_Click);
+            //gr = tempBar.CreateGraphics();
+            // bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            //req(null, null);
+        }
+
+       
+
+        
+
+        public void msg(string mesg)
+        {
+           // textBox1.Text = Environment.NewLine + " >> " + mesg;
+            textBox1.Invoke(new Action(()=> { textBox1.Text = Environment.NewLine + " >> " + mesg; }));
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            
+                clientSocket = new System.Net.Sockets.TcpClient();
+                msg("Connecting...");
+                toolStripStatusLabel1.Text = "Connecting...";
+
+
+
+                try
+                {
+                    tcp_port = (int)portNum.Value;
+                    IPadd = textHostName.Text;
+
+                  
+                   //clientSocket.Connect(IPadd, tcp_port);
+                var result = clientSocket.BeginConnect(IPadd, tcp_port, null, null);
+                var success = result.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(4));
+                if (!success)
+                {
+                    throw new Exception("Failed to connect.");
+                }
+
+                  serverStream = clientSocket.GetStream();
+                  toolStripStatusLabel1.Text = "Client Socket Program - Server Connected ...";
+                
+
+            }
+                catch (Exception ex) { toolStripStatusLabel1.Text = ex.Message; return; }
+
+                try
+                {
+                   // serverStream = clientSocket.GetStream();
+                    if (radioBtnOn.Checked) str = "LED=ON";
+                    if (radioBtnOff.Checked) str = "LED=OFF";
+                    if (radioBtnAuto.Checked) str = "LED=Auto "+setTemp.Value.ToString();
+                //outStream = Encoding.UTF8.GetBytes("GET /HTTP/ 1.1 /" + str + "\r\n");
+                outStream = Encoding.UTF8.GetBytes(" /"+str + "\r\n");
+                   serverStream.Write(outStream, 0, outStream.Length);
+                    serverStream.Flush();
+                    byte[] inStream = new byte[100025];
+                    Console.WriteLine("-----------"+ (int)clientSocket.ReceiveBufferSize);
+                    serverStream.Read(inStream, 0, (int)clientSocket.ReceiveBufferSize);
+                    string returndata = Encoding.UTF8.GetString(inStream);
+                    if (returndata.IndexOf("ON") != -1) on_off = true;
+                    else on_off = false;
+                    Regex reg = new Regex(@"\d+");
+                    MatchCollection matches = reg.Matches(returndata);
+                if (matches.Count == 2)
+                {
+                    t = int.Parse(matches[0].Value);
+                    h = int.Parse(matches[1].Value);
+                }
+                if (matches.Count == 3)
+                {
+                    t = int.Parse(matches[1].Value);
+                    h = int.Parse(matches[2].Value);
+                }
+                msg(returndata);
+                clientSocket.Close();
+                //Picture box
+
+                bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                pictureBox1.Image = null;
+                Graphics gr = Graphics.FromImage(bitmap);
+                // gr.FillRectangle(Brushes.Red, 10, 180-2*t, 5, 2*t);
+                // gr.FillRectangle(Brushes.Blue, 50, 180 - 2*h, 5, 2*h);
+                    gr.DrawEllipse(Pens.Black, 73, 23, 39, 39);
+                    gr.DrawEllipse(Pens.Black, 73, 113, 39, 39);
+                if (on_off)
+                {
+                    gr.FillEllipse(Brushes.Green, 75, 25, 35, 35);                    
+                }
+                else
+                {
+                    gr.FillEllipse(Brushes.Black, 75, 115, 35, 35);                    
+                }
+                pictureBox1.Image = bitmap;
+                //Chart area
+                chart1.Series["Temperature"].Points.Clear();
+                chart1.Series["Humidity"].Points.Clear();
+                chart1.Series["Temperature"].Points.AddXY("T1",t);
+               // chart1.Series["Temperature"].Points.AddXY("T2", h);
+               // chart1.Series["Humidity"].Points.AddXY("H1", t);
+                chart1.Series["Humidity"].Points.AddXY("H2", h);
+
+            }
+
+                catch (Exception ex) {  toolStripStatusLabel1.Text= ex.Message; }
+            
+        }
+
+              
+        private void ProgramClose(Object sender, FormClosingEventArgs e)
+        {
+          try {
+                NetworkStream serverStream = clientSocket.GetStream();
+                outStream = Encoding.UTF8.GetBytes("GET /HTTP/ 1.1 /LED=DISC\r\n");
+                serverStream.Write(outStream, 0, outStream.Length);
+                serverStream.Flush();
+              }
+               catch { }
+        }
+
+        private void portNum_ValueChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("Port Changed", "Warning");
+        }
+
+        private void btnSerial_Click(object sender, EventArgs e)
+        {
+            s_conn = new Serial();
+            s_conn.ShowDialog();
+        }
+
+        private void btnMonitor_Click(object sender, EventArgs e)
+        {
+
+           if (!mon)
+            {
+                
+                btnMonitor.Text = "Stop";
+                mon = true;
+            }
+            else
+            {
+                
+                btnMonitor.Text = "Mon";
+                mon = false;
+            }
+             new Task(()=> {
+               while (mon)
+               {
+                Console.Beep();
+                req(null, null);
+                System.Threading.Thread.Sleep(10000);
+               }
+
+            }).Start();
+
+ 
+
+        }
+    }
+}
